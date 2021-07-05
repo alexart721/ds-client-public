c25d2a9c3011ddcbe7826ad6ecb5d85b1f3bd1a0
Alex Teodorescu <ateodorescu721@gmail.com>
2021-07-05 10:56:49 -0400

    fix routing to auth side on login page

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -22,13 +22,13 @@ const Login: React.FC = () => {
 
   const onFinish = async (values: UserLogin) => {
     try {
-      console.log(values);
-
-      const accessToken = await loginUser(values).then(res => res.json());
-      console.log(accessToken);
-
+      const { accessToken } = await loginUser(values).then(res => res.json());
       localStorage.setItem('accessToken', accessToken);
-      router.push('http://localhost:3000');
+      // router.push(`http://localhost:3000/${accessToken}`);
+      router.push({
+        pathname: 'http://localhost:3000/',
+        query: { accessToken },
+      });
     } catch (error) {
       console.error(error);
     }
